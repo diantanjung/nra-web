61b3bd4d621ec7d4a652e21178309edd4d2a2789
MirrorBottle <setiawanbayu66152@gmail.com>
2023-01-27 11:31:58 +0800

    fix: import styling

--- src/views/admin/Import/IndexView.vue
+++ src/views/admin/Import/IndexView.vue
@@ -36,15 +36,26 @@ onBeforeUnmount(() => {
 
 @import "dropzone/dist/dropzone.css";
 @import "@/assets/scss/vendor/dropzone";
+
+#import-index-view {
+  .dz-default.dz-message {
+    display: none!important;
+  }
+}
 </style>
 
 <template>
   <!-- Hero -->
   <BasePageHeading title="Import" />
   <!-- Page Content -->
-  <div class="content">
-    <BaseBlock title="Import Data Excel" class="pb-4">
-        <form id="dropzoneForm" class="dropzone"></form>
+  <div class="content" id="import-index-view">
+    <BaseBlock class="pb-4">
+        <form id="dropzoneForm" class="dropzone">
+          <div class="dz-message d-flex flex-column align-items-center justify-content-center" data-dz-message>
+            <i class="fa fa-file-import text-success fa-5x"></i>
+            <h5 class="mt-3">Upload file dengan format <b>.xlsx</b></h5>
+          </div>
+        </form>
     </BaseBlock>
   </div>
 </template>
